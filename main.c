ddc5e8e95a1957a36225cfc6dc19c256b385081a
cham-s <cattouma@student.42.fr>
2016-05-04 22:44:24 +0200

    started open cl

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <Opencl/opencl.h>
+
+#define DATA_SIZE (1024)
+
+// Simple compute kernel which computes the square of an input array 
+// //
+ const char *KernelSource = "\n" \
+"__kernel void square(                                                       \n" \
+"   __global float* input,                                              \n" \
+"   __global float* output,                                             \n" \
+"   const unsigned int count)                                           \n" \
+"{                                                                      \n" \
+"   int i = get_global_id(0);                                           \n" \
+"   if(i < count)                                                       \n" \
+"       output[i] = input[i] * input[i];                                \n" \
+"}                                                                      \n" \
+"\n";
+
+int main()
+{
+	printf("hellow\n");
+}
